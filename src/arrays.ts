6cb08eb153fd7b41556fcf96b993f353ca722784
JeanaeClark <jeclark@udel.edu>
2022-09-21 13:06:19 -0400

    Completed stringsToIntegers and passed tests

--- src/arrays.ts
+++ src/arrays.ts
@@ -31,7 +31,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const numbersclone = numbers.map((element: string): number =>
+        Number(element) ? Number(element) : Number("0")
+    );
+    return numbersclone;
 }
 
 /**
